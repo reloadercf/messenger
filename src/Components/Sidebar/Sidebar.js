689f602a44c153b4a4749228a3db6439aa1e4091
reloadercf <elcfm1@gmail.com>
2020-10-05 16:56:37 -0500

    start

---
+++ src/Components/Sidebar/Sidebar.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import {Avatar} from "@material-ui/core"
+import DonutLargeIcon from '@material-ui/icons/DonutLarge';
+import ChatIcon from '@material-ui/icons/Chat'; 
+import MoreVertIcon from '@material-ui/icons/MoreVert';
+
+import './Sidebar.css'
+
+function Sidebar() {
+    return (
+        <div className="sidebar">
+            <div className="sidebar__header">
+                <Avatar />
+                <div className="sidebar__headerRight">
+                    <DonutLargeIcon />
+                    <ChatIcon />
+                    <MoreVertIcon />
+                </div>  
+            </div>
+            <div className="sidebar__search">
+                
+            </div>
+            <div className="sidebar__chats">
+
+            </div>
+        </div>
+    )
+}
+
+export default Sidebar
---
+++ src/firebase.js
@@ -0,0 +1,11 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyD-g6lIT2fucZtHbGgfmc2Wk_6fHRAwI5A",
+    authDomain: "selah-288.firebaseapp.com",
+    databaseURL: "https://selah-288.firebaseio.com",
+    projectId: "selah-288",
+    storageBucket: "selah-288.appspot.com",
+    messagingSenderId: "857074817012",
+    appId: "1:857074817012:web:c7c1b7d3258b2e42fb4ba1",
+    measurementId: "G-XSCC3GNRPH"
+  };
